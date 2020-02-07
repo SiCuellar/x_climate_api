358ec13a644965ac85ba5333ceb6e185141c787c
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-06 21:28:11 -0800

    functioning api service

--- spec/services/darksky_service_spec.rb
+++ spec/services/darksky_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "darksky service" do
   it "can return specific weather data for specfic coordinates" do
-
       city_forecast = DarkskyService.new.get_forcast(39.7392358, -104.990251)
+      expect(city_forecast).to be_a(Hash)
   end
 end
