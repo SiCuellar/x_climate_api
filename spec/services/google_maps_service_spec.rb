0c6beac0831fcea588384dbbae61fe0a36d032a8
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-11 22:27:01 -0700

    adding google map service for coordinates

---
+++ spec/services/google_maps_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe GoogleMapsService do
+  it " Gets JSON data" do
+
+    location = "denver,co"
+    service = GoogleMapsService.new
+    coordinates = service.get_coordinates(location)
+
+    expect(service).to be_an_instance_of(GoogleMapsService)
+    expect(coordinates).to eq({:lat=>39.7392358, :lng=>-104.990251})
+  end
+end
