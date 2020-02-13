ff9aebb98d5b0f4e31c5c341d927616fe6430f61
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-12 21:04:14 -0700

    added testing for hourly weather and daily weather

--- spec/poros/forecast_organizer_spec.rb
+++ spec/poros/forecast_organizer_spec.rb
@@ -8,4 +8,22 @@ describe 'forecast organizer' do
     expect(forecast_organizer.current_weather).to be_an_instance_of(CurrentWeather)
     expect(forecast_organizer.current_weather).to be_an_instance_of(CurrentWeather)
   end
+
+  it "#daily_weather" do
+    location = "Denver, CO"
+    forecast_organizer = ForecastOrganizer.new(location)
+
+    expect(forecast_organizer.daily_weather).to be_a(Array)
+    expect(forecast_organizer.daily_weather[0]).to be_an_instance_of(DayWeather)
+    expect(forecast_organizer.daily_weather.count).to eq(7)
+  end
+
+  it "#hourly_weather" do
+    location = "Denver, CO"
+    forecast_organizer = ForecastOrganizer.new(location)
+
+    expect(forecast_organizer.hourly_weather).to be_a(Array)
+    expect(forecast_organizer.hourly_weather[0]).to be_an_instance_of(HourWeather)
+    expect(forecast_organizer.hourly_weather.count).to eq(8)
+  end
 end
