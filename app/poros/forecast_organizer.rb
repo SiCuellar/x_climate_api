243d1e1884e0b9c33e2425e81a86c3d3356ba555
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-12 21:05:23 -0700

    added daily weather methods and hourly methods

--- app/poros/forecast_organizer.rb
+++ app/poros/forecast_organizer.rb
@@ -10,4 +10,16 @@ class ForecastOrganizer
   def current_weather
     CurrentWeather.new(@weather_data[:currently])
   end
-end 
+
+  def daily_weather
+    @weather_data[:daily][:data].map do |day_data|
+      DayWeather.new(day_data)
+    end.first(7)
+  end
+
+  def hourly_weather
+    @weather_data[:hourly][:data].map do |hour_data|
+      HourWeather.new(hour_data)
+    end.first(8)
+  end
+end
