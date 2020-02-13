963ef385b86be1feba575a149cd7c3ac679125cb
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2020-02-12 21:00:42 -0700

    testing for weather organizer

---
+++ spec/poros/forecast_organizer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'forecast organizer' do
+  it "can organize all inforamtion needed for a forecaset page" do
+      location = "Denver, CO"
+      forecast_organizer = ForecastOrganizer.new(location)
+
+      expect(forecast_organizer.current_weather).to be_an_instance_of(CurrentWeather)
+      expect(forecast_organizer.current_weather).to be_an_instance_of(CurrentWeather)
+    end
+  end
+end
